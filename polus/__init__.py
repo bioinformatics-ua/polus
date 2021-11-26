eef4d22db4ad5a5853d9df88d4f673c9ebf3b478
T-Almeida <tiagomeloalmeida@ua.pt>
2021-11-26 13:50:31 +0000

    hyperlimks?

--- polus/__init__.py
+++ polus/__init__.py
@@ -19,15 +19,15 @@ retrieval) tasks.
 
 The main API consists of:
 
-- **training.py**: Training API that contains the most basic training loop
-- **data.py**: DataLoaders API that extends the tf.data.Dataset functionality
+- `polus.training.py`: Training API that contains the most basic training loop
+- `polus.data.py`: DataLoaders API that extends the tf.data.Dataset functionality
  to build more useful data loaders with easy to use caching mechanisms.
-- **callbacks.py**: Main source of interaction with the main training loop
-- **models.py**: Model API define an extension of the tf.keras.Model by handling
+- `polus.callbacks.py`: Main source of interaction with the main training loop
+- `polus.models.py`: Model API define an extension of the tf.keras.Model by handling
  storing and loading of the entire model architecture.
-- **metrics**: Metrics API describe how metrics should be implemented so that can
+- `polus.metrics`: Metrics API describe how metrics should be implemented so that can
  be efficiently used during the training loop.
-- `**core**: The Core API defines some base classes or variables that are used
+- `polus.core`: The Core API defines some base classes or variables that are used
  through the framework. It also exposes some functions to change the internal
  behaviour of the polus framework, e.g., the use of XLA.
  
