c924f30436aded666982c2158e742f0f034e4b75
T-Almeida <tiagomeloalmeida@ua.pt>
2021-11-26 12:45:43 +0000

    core documentation

--- polus/core.py
+++ polus/core.py
@@ -2,17 +2,15 @@ r'''
 
 # Polus core 
 
-Here we describe operations that are
-used through all packages of this toolkit
+Here we describe operations that are used through all packages 
+of this toolkit.
 
-Furtheremore, the package core have module-wide
-variables that govern some internal behaviour of
-this framework.
-
-For instance, polus.core has an jit_compile attribute
-that controls the utilization of XLA during tf.function,
-the correct way to write or read this attribute is throug 
-its associated methods:
+Furthermore, the package core has module-wide variables that 
+govern some internal behaviour of this framework. For instance, 
+polus.core has a jit_compile attribute that controls 
+the utilization of XLA during the code conversion to static 
+computational graphs, the correct way to write or read this 
+attribute is through its associated methods:
 
 ```python
 
@@ -33,28 +31,58 @@ import sys
 import tensorflow as tf
 from logging.handlers import TimedRotatingFileHandler
 
-FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+
 
 # module-wide variables
 this = sys.modules[__name__]
 this.jit_compile=True
     
-def set_jit_compile(mode):
+def set_jit_compile(mode: bool):
+    """
+    Changes the *jit_compile* attribute.
+    
+    Args:
+      mode (bool): Boolean variable that defines the utilization of
+        jit_compiler during build of computation graphs, which enables
+        XLA compiler.
+        
+    Returns:
+      None
+    """
     this.jit_compile=mode
 
 def get_jit_compile():
+    """
+    Reads the value of the *jit_compile* attribute.
+    """
     return this.jit_compile
     
 class BaseLogger:
     def __init__(self, logging_level=logging.DEBUG, log_name="polus.log"):
         """
-        From: https://www.toptal.com/python/in-depth-python-logging
+        Base logging class, this sets a console and file log handler to each
+        instance. Meaning that each call to the logger will write to both 
+        handlers. 
+        
+        Furthermore, the intended behaviour is to classes to extend this BaseLogger
+        classe and by doing this, any class can access to the logger property
+        Note that multiple instances use the same handler
+        
+        Main ideas from: https://www.toptal.com/python/in-depth-python-logging
         """
         super().__init__()
-
+        
+        # Note by: Tiago
+        # This is a bad solution if the framework scales to a lot of
+        # instance during the training of a object.
+        # If needed consider to change to a more managle behaviour
+        
         self.logger = logging.getLogger(self.__class__.__name__)
         
         if not self.logger.hasHandlers():
+            
+            FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+            
             self.logger.setLevel(logging_level)
 
             console_handler = logging.StreamHandler(sys.stdout)
@@ -71,13 +99,33 @@ class BaseLogger:
             self.logger.propagate = False
     
     def set_logging_level(self, logging_level):
+        """
+        
+        Args:
+          logging_level (int): A valid logging level supperted by the
+            logging package, see https://docs.python.org/3/library/logging.html#logging-levels
+            
+        Returns:
+          None
+        
+        """
         self.logger.setLevel(logging_level)
         
         
 def find_dtype_and_shapes(data_generator, k=5):
     """
-    Automatically gets the dtype and shapes of samples
+    Automatically infer the data type and shapes of the samples from 
+    the *data_generator*. This function is highly used in polus.data.
     
+    Args:
+      data_generator (python generator): python generator that output
+        samples in a **dictionary** format.
+        
+      k (int): Number of samples that are output from *data_generator*
+        in order to correctly infer the shape of each dictionary value.
+        For instance, if a sample contains a dynamic shape, this function
+        will return None in the place of the dimension that as a dynamic 
+        shape.
     """
     # get one sample
     assert k>0
@@ -121,6 +169,23 @@ def find_dtype_and_shapes(data_generator, k=5):
 
 
 def execute_if(condition_var, error_message="", on=True):
+    """
+    A decorated function that restricts the execution of a specific
+    function based on a condition. This is useful for instance methods
+    that we only want to be called based on a simple condition.
+    
+    Args:
+      condition_var (boolean): Boolean expression that will be evaluated
+        when the decorated method is called.
+      
+      error_message (str): A printable error message to be displayed when
+        the condition fails.
+        
+      on (boolean): The condition that the *condition_var* is evaluated
+        against, by default the decorated method executes if *condition_var*
+        is True.
+    
+    """
     def decorator(func):
         def function_wrapper(self, *args, **kwargs):
 
