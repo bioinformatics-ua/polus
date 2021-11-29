d303d0b0a4140fd9d9ff83694e270088328094c7
T-Almeida <tiagomeloalmeida@ua.pt>
2021-11-29 12:24:15 +0000

    backwards compatibility

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ except ImportError: # for pip <= 9.0.3
 import polus
 install_reqs = parse_requirements("requirements.txt", session=False)
 
+# https://stackoverflow.com/questions/62114945/attributeerror-parsedrequirement-object-has-no-attribute-req
+requirements = list(requirements) 
+try:
+    requirements = [str(ir.req) for ir in install_reqs]
+except:
+    requirements = [str(ir.requirement) for ir in install_reqs]
+
 setup(
     name='polus',
     packages=find_packages(include=['polus*']),
@@ -14,7 +21,7 @@ setup(
     author='Tiago Almeida and Rui Antunes',
     author_email='tiagomeloalmeida@ua.pt',
     license='MIT',
-    install_requires=[str(ir.req) for ir in install_reqs],
+    install_requires=requirements,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
