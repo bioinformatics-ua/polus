7feec07d637b511519433cc2f045baa32a1e9160
T-Almeida <tiagomeloalmeida@ua.pt>
2021-11-29 12:21:01 +0000

    backwards compatibility

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 import polus
 install_reqs = parse_requirements("requirements.txt")
 
