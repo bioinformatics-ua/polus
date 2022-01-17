32a85affeb341594adde364bfa2e5b97f762da07
T-Almeida <tiagomeloalmeida@ua.pt>
2022-01-17 13:30:11 +0000

    removing jit_compile from the train loop

--- polus/training.py
+++ polus/training.py
@@ -126,7 +126,7 @@ class BaseTrainer(BaseLogger):
         """
         raise NotImplementedError("foward_with_grads function must be implemented in order to compute a loss value for optimization")
     
-    @tf.function(jit_compile=get_jit_compile())#()
+    @tf.function#()
     def train_step(self, *inputs):
         """
         Describes a static computation tensorflow graph that
